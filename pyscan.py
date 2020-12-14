feb8f3effe7ec86fbd699ee4002241039ba576d6
Zaragon-AQ <40393473+Zaragon-AQ@users.noreply.github.com>
2020-12-14 20:38:09 +0100

    feat(port): add multiple port

--- pyscan.py
+++ pyscan.py
@@ -1,24 +1,26 @@
 import socket
 import argparse
 
-def scan(ip, port):
-     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     result = s.connect_ex((ip, port))
 
-     if result == 0:
-         print('Port '+ ip +':' +str(port)+ ' is open') 
-     else: 
-         print('Port '+ ip +':' +str(port)+ ' is closed') 
+def scan(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+    result = s.connect_ex((ip, port))
+
+    if result == 0:
+        print('Port ' + ip + ':' + str(port) + ' is open')
+    else:
+        print('Port ' + ip + ':' + str(port) + ' is closed')
+
+    s.close()
 
 
 if __name__ == '__main__':
-
     ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--ip", required=True, help="host or ip")
+    ap.add_argument("-p", "--port", required=True, help="one or more ports with ',' separator")
 
-    ap.add_argument("-i", "--ip", required=True, help= "host or ip")
-    ap.add_argument("-p", "--port", required=True, help= "port")
     args = vars(ap.parse_args())
-    ip = args["ip"]
-    port = int(args["port"])
- 
-    scan(ip, port)
+    ports = args["port"].split(",")
+    for p in ports:
+        scan(args["ip"], int(p))
